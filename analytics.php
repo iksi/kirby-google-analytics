81264b283897c6dcc066b5887e6d40be4a59e7c5
Iksi <jur@iksi.cc>
2014-12-30 14:33:31 +0100

    Update analytics.php

--- analytics.php
+++ analytics.php
@@ -12,13 +12,13 @@ function analytics()
     
     // Get it from a site field
     $analytics = site()->analytics();
-    
-    $data = array();
 
-    if ( ! $analytics->empty() && ! in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+    if ($analytics->empty() || in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
     {
-        $data['analytics'] = $analytics;
+        return FALSE;
     }
 
-    return tpl::load(__DIR__ . DS . 'template.php', $data);
+    return tpl::load(__DIR__ . DS . 'template.php', array(
+        'analytics' => $analytics->html()
+    ));
 }
