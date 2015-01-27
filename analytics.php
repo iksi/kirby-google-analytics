1dbe7cd5e58e4f5b5ec1165dcdff5066f8fcb2af
iksi <jur@iksi.cc>
2015-01-27 16:40:05 +0100

    Version bump

--- analytics.php
+++ analytics.php
@@ -3,11 +3,10 @@
  * Analytics plugin for Kirby
  *
  * @author Iksi <info@iksi.cc>
- * @version 1.0.5
+ * @version 1.1
  */
 function analytics()
 {
-    // Get it from config
     $analytics_id = c::get('google.analytics_id', false);
 
     if ( ! $analytics_id || in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
