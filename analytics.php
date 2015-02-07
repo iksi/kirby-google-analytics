36c07e32473bac6e73e57045f7da9ebc766345e5
iksi <jur@iksi.cc>
2015-02-08 00:20:42 +0100

    added comments

--- analytics.php
+++ analytics.php
@@ -7,11 +7,14 @@
  */
 function analytics()
 {
+    // Get the analytics_id from config
     $analytics_id = c::get('google.analytics_id', false);
 
     if ( ! $analytics_id || in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+        // No id given or on localhost
         return false;
     }
 
+    // Return the template
     return tpl::load(__DIR__ . DS . 'template.php', compact('analytics_id'));
 }
